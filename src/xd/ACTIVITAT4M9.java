0c4fc2136d86e9c26bbd622b9243e74c8daf55e7
Alumne <Alumne@104-PC12>
2019-11-07 16:25:28 +0100

    Segon Commit

--- src/xd/ACTIVITAT4M9.java
+++ src/xd/ACTIVITAT4M9.java
@@ -1,13 +1,16 @@
 package xd;
 
 import java.util.Scanner;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class ACTIVITAT4M9 {
 
 	public static void main(String[] args) {
 	
+		ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(3);
 		
-	//p
+				
 		
 	}
 	
